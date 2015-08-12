7c87067c361f0d2b5c199830d271646dbe47c0b9
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-08-12 17:24:49 +0200

    More details in homepage.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,42 @@
-module StaticPagesHelper
+module StaticPagesHelper  
+  def my_name
+    'Scott Zhou'
+  end
+  
+  def my_birthday
+    'October 1980'
+  end
+
+  def my_gender
+    'Male'
+  end
+  
+  def my_marital_status
+    'Married'
+  end
+  
+  def my_nationally
+    'Chinese'
+  end
+  
+  def my_location
+    'Stockholm, Sweden'
+  end
+  
+  def my_language
+    'English, Chinese, and little Swedish'
+  end
+  
+  def my_tel
+    '+46 725398656'
+  end
+
+  def my_email
+    'scott.cong.zhou@gmail.com'
+  end
+  
+  def my_github_homepage
+    "https://github.com/scott-zhou/"
+  end
+  
 end
