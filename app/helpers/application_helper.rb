ce4e8064309786b4ad783ed1fd1395bf95d2b54d
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-08-18 11:27:41 +0200

    Add link tag for a favicon in HTML head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def full_title(page_title = '')
     base_title = "Scott Zhou"
     if page_title.empty?
-      base_title
+      "#{base_title}'s Homepage"
     else
       "#{page_title} | #{base_title}"
     end
